142d17ca139180b53848ad34f118c239bef4e8b0
probir ghosh <probirghosh@outlook.com>
2023-04-24 14:19:15 +0600

    daisyui: default theme & customized theme applied : hammer:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 const App = () => {
   return (
     <>
-       <p className="text-3xl font-bold underline">Hello React!</p>    
+      <div>
+        <p className="text-3xl font-bold underline bg-secondary">
+          Hello React!
+        </p>
+        <button className="btn btn-primary">Click Me</button>
+      </div>
+      {/* use a theme specific section only*/}
+      {/* <div data-theme="light">
+        This div will always use light theme
+        <span data-theme="retro">This span will always use retro theme!</span>
+      </div> */}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    // themes: false,
+    themes: [
+      {
+        mytheme: {
+          primary: "#14532d",
+
+          secondary: "#84cc16",
+
+          accent: "#cb28ef",
+
+          neutral: "#1C1929",
+
+          "base-100": "#E3ECF3",
+
+          info: "#9EC7F0",
+
+          success: "#24DBB4",
+
+          warning: "#9B6C0D",
+
+          error: "#FA6B7B",
+        },
+      },
+      // "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
+    ],
   },
-  plugins: [],
-}
+};
